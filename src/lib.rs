94dfc9a0f8613312ac38ffe6480040be50221fd6
jheryer <jheryer@gmail.com>
2023-04-08 10:19:59 -0500

    basic section parser rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,14 @@ pub fn run(sheets: Vec<String>, list: bool) -> RunResult<()> {
         let path = get_file_from_name(file_sheet.to_string()).into_owned();
         match open_file(&path) {
             Err(err) => eprintln!("{}: {}", path, err),
-            Ok(file) => process_sheet(file),
+            Ok(file) => process_new_sheet(file),
         }
     }
 
     Ok(())
 }
 
+// test this
 fn get_file_from_name<'a>(name: String) -> Cow<'a, str> {
     let path = format!("./tests/inputs/{}.md", name);
     Cow::Owned(path)
@@ -32,44 +33,67 @@ fn open_file(filename: &str) -> RunResult<Box<dyn BufRead>> {
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
     }
 }
+#[derive(Debug)]
+struct MDSection {
+    depth: usize,
+    content: Vec<String>,
+}
 
-fn process_sheet(reader: Box<dyn BufRead>) {
-    let markdown = reader
-        .lines()
-        .map(|line| line.unwrap())
-        .collect::<Vec<String>>()
-        .join("\n");
-    let parser = Parser::new(&markdown);
-    #[allow(unused)]
-    let mut in_heading = false;
-    let mut in_section = false;
-    let mut current_level = 0;
-
-    for event in parser {
-        match event {
-            #[allow(unused)]
-            Event::Start(Tag::Heading(level, ..)) => {
-                in_heading = true;
-                in_section = true;
-                current_level = level as i32;
-            }
-            #[allow(unused)]
-            Event::End(Tag::Heading(..)) => {
-                println!("\n");
-                in_heading = false;
-            }
-            #[allow(unreachable_patterns)]
-            Event::Start(Tag::Heading(new_level, ..))
-                if in_section && new_level as i32 <= current_level =>
-            {
-                in_section = false;
-            }
-            Event::Text(text) => {
-                if in_section {
-                    print!("{}", text);
-                }
-            }
-            _ => {}
+impl MDSection {
+    fn new() -> MDSection {
+        MDSection {
+            depth: 0,
+            content: Vec::new(),
         }
     }
+
+    fn new_section(depth: usize) -> MDSection {
+        MDSection {
+            depth: depth,
+            content: Vec::new(),
+        }
+    }
+
+    fn add_line(&mut self, line: String) {
+        self.content.push(line);
+    }
+}
+
+fn process_new_sheet(sheet: Box<dyn BufRead>) {
+    let mut parsed_sheet = Vec::<MDSection>::new();
+    parsed_sheet.push(MDSection::new());
+    for (line_num, line) in sheet.lines().enumerate() {
+        let line = line.unwrap();
+        let new_section = section_check(&line);
+        match new_section {
+            Some(mut section) => {
+                section.add_line(String::from(&line));
+                parsed_sheet.push(section);
+            }
+            _ => {
+                if let Some(last) = parsed_sheet.last_mut() {
+                    last.add_line(String::from(&line));
+                }
+            }
+        }
+    }
+
+    println!("{:?}", parsed_sheet)
+}
+
+fn section_check(line: &str) -> Option<MDSection> {
+    let mut depth = 0;
+
+    for ch in line.chars() {
+        match ch {
+            '#' => depth += 1,
+            _ => break,
+        }
+    }
+
+    if depth == 0 {
+        return None;
+    }
+
+    Some(MDSection::new_section(depth))
 }
---
+++ tests/inputs/test.md
@@ -0,0 +1,32 @@
+# section1
+
+## section1 2
+
+## section1 2.1
+
+### section1 2.1.1
+
+### section1 2.1.2
+
+# section 2
+--------
+
+### section 1
+
+## section 2
+
+----
+
+section
+
+ search section by default , and include any headers that are at a lower level
+
+ 1,2,3,4,5,6
+
+ [
+   Section(depth, content),
+   Section(depth, content),
+   Section(depth, content),
+   Section(depth, content),
+   Section(depth, content),
+ ]
